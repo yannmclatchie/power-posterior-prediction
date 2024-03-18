4f4c070f6a05a7e087985d87b4a19e59bb186792
Yann McLatchie <yann@mclatchie.com>
2024-03-18 17:52:34 +0000

    update plotting scripts

--- R/normal-location-misspecified/plotting-elpd.R
+++ R/normal-location-misspecified/plotting-elpd.R
@@ -4,59 +4,52 @@ library(tidyverse)
 library(simstudy)
 source("R/normal-location-misspecified/config.R")
 
+## data concatenation
 #files <- list.files("data/normal-location-misspecified-elpd", full.names = TRUE)
 #df <- files %>%
 #  map(read_csv) %>% 
 #  reduce(rbind)
 #write_csv(df, "data/normal-location-misspecified-elpd/normal-location-misspecified-elpd-all.csv")
+
+# data reading
 df <- read_csv("data/normal-location-misspecified-elpd/normal-location-misspecified-elpd-all.csv")
 
 # fix ordering of priors
 df$prior = factor(df$prior, levels=c('weak', 'flat'))
 
-# Group dataframe by iteration
-gdf <- df |>
-  group_by(n, tau, prior) |>
-  summarize(elpd_mean = mean(elpd))
-
-# Identify optimal tau
-best_lines <- gdf |>
-  ungroup() |>
-  group_by(n, prior) |>
-  filter(elpd_mean == max(elpd_mean)) 
-
 # Produce ribbons for the figures
 rdf <- df |>
   group_by(tau, n, prior) |>
-  summarize(elpd_min = quantile(elpd, probs = 0.1),
-            elpd_max = quantile(elpd, probs = 0.9))
+  summarize(elpd_min = quantile(elpd, probs = 0.05),
+            elpd_max = quantile(elpd, probs = 0.95))
+
+# Reduced number of iterations
+df_100 <- df |>
+  filter(iter <= 50)
 
 # Plot the elpd over iterations
 p_elpd <- ggplot() +
+  geom_line(data = df_100,
+            aes(tau, elpd, group = iter), 
+            colour = "grey",
+            #size = 0.2, 
+            alpha = 0.2) +
   geom_ribbon(data = rdf,
               aes(ymin = elpd_min,
                   ymax = elpd_max,
                   x = tau),
-              colour = "grey",
+              colour = "black",
               alpha = 0.,
+              #size = 0.5,
               linetype = "dotted") +
-  geom_line(data = gdf,
-            aes(tau, elpd_mean),
-            colour = "black",
-            size = 1) +
-  geom_vline(xintercept = 1,
-             linewidth = 0.25,
-             linetype = "dashed") +
-  facet_grid(prior ~ n, scales = "free_y") +
-  scale_x_continuous(trans = "log10", breaks = 10^seq(-4, 4, length.out = 3)) +
+  geom_vline(xintercept = 1, linetype = "dashed") +
+  facet_grid(prior ~ n, scales = "fixed") +
+  scale_x_continuous(trans = "log2", 
+                     breaks = c(0.01, 0.1, 1, 10, 100),
+                     label = function(x) ifelse(x == 0, "0", x)) +
   xlab("tau") +
-  ylab("elpd(tau)") +
-  theme_bw() +
-  theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        strip.background = element_blank(),
-        panel.background = element_blank(),
-        legend.position="none") 
+  ylab("LOO-CV elpd") +
+  paper_theme
 p_elpd
 
 # save the plot
